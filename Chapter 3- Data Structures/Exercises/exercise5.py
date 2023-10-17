30031b47ba87d2a4c12af436aa4ef57913169070
maryelgregoreo <maryelgregorio55@gmail.com>
2023-10-17 12:27:04 +0400

    exercise5.py

---
+++ Chapter 3- Data Structures/Exercises/exercise5.py
@@ -0,0 +1,17 @@
+## Exercise 5: Change Guest List :ballot_box_with_check:
+
+#Inviting to dinner
+
+guests = ["Chaewon", "Yunjin", "Kazuha"]
+
+print(f'Hey! Can I take you out to dinner, {guests [0]}?')
+print(f'{guests [1]}! Want to go to dinner with me?')
+print(f'{guests [2]}, Would you like to go to dinner with me?')
+
+print(f'Oh! {guests [0]} cannot make it to dinner, she is sick.. Let us invite Sakura instead!')
+
+#Chaewon can't make it so let's invite Sakura
+
+guests [0] = "Sakura"
+
+print(f'Hey {guests [0]}! Chaewon cannot make it to dinner, would you like to come instead?')

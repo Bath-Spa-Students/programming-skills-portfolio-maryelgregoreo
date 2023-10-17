dc9a39fdb59b1a25a0badb73b63ac2775bedc861
maryelgregoreo <maryelgregorio55@gmail.com>
2023-10-17 12:27:28 +0400

    exercise7.py

---
+++ Chapter 3- Data Structures/Exercises/exercise7.py
@@ -0,0 +1,23 @@
+## Exercise 7: Seeing the World :ballot_box_with_check:
+
+locations = ["Han River", "HYBE Building", "Boracay", "Tokyo Tower", "Ghibli Museum" ]
+
+print(f'Original order: \n {locations}')
+
+print(f'Alphabetical: \n{sorted (locations)}')
+
+print(f'Original order: \n {locations}')
+
+print(f'Reverse Alphabetical: \n{sorted (locations, reverse=True)}')
+
+print(f'Original order: \n {locations}')
+
+locations.reverse()
+print(f'Reversed: \n {locations}')
+
+locations.reverse()
+print(f'Original order: \n {locations}')
+
+print(f'Alphabetical: \n{sorted (locations)}')
+
+print(f'Reverse Alphabetical: \n{sorted (locations, reverse=True)}')

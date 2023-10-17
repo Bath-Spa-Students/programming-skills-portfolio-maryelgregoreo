bff9764a1d90dddca32857569b5de0a9ee3bfbac
maryelgregoreo <maryelgregorio55@gmail.com>
2023-10-17 12:26:26 +0400

    exercise1.py

--- Chapter 3- Data Structures/Exercises/exercise1.py
+++ Chapter 3- Data Structures/Exercises/exercise1.py
@@ -1,8 +1,5 @@
 ## Exercise 1: Names :ballot_box_with_check:
 
-""""Store the names of a few of your friends in a list called names. Print each person's name by accessing 
-each element in the list, one at a time."""
-
 name = ['Alekz', 'Sam', 'Seif', 'Mariez']
 
 print (((name [0],

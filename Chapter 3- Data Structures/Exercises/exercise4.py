2e53de2f28a394e0a2b7ea916f0c3df115e9a13b
maryelgregoreo <maryelgregorio55@gmail.com>
2023-10-17 12:26:56 +0400

    exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/exercise4.py
@@ -0,0 +1,7 @@
+## Exercise 4: Guest List :ballot_box_with_check:
+
+guests = ["Chaewon", "Yunjin", "Kazuha"]
+
+print(f'Hey! Can I take you out to dinner, {guests [0]}?')
+print(f'{guests [1]}! Want to go to dinner with me?')
+print(f'{guests [2]}, Would you like to go to dinner with me?')

dfeacae95e05de5cc958077f9361b3f9a00c59a6
maryelgregoreo <maryelgregorio55@gmail.com>
2023-10-17 12:26:43 +0400

    exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/exercise3.py
@@ -0,0 +1,7 @@
+##Exercise 3: Your Own List :ballot_box_with_check:
+
+transportation = ['car', "airplane", "motorcycle"]
+
+print(f'I want to drive a {transportation [0]}.')
+print(f'I want to ride an {transportation [1]} again.')
+print(f'I want to learn how to ride a {transportation [2]}.')

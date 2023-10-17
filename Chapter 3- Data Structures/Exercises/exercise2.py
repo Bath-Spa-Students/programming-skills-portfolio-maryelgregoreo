5da51b4d8a84dbe42c6f348d4c26e3869bec14eb
maryelgregoreo <maryelgregorio55@gmail.com>
2023-10-17 12:26:34 +0400

    exercise2.py

--- Chapter 3- Data Structures/Exercises/exercise2.py
+++ Chapter 3- Data Structures/Exercises/exercise2.py
@@ -1,9 +1,5 @@
 ## Exercise 2: Greetings :ballot_box_with_check:
 
-"""Start with the list you used in Exercise 1, but instead of just printing each person's name, 
-print a message to them. The text of each message should be the same, but each message should be 
-personalized with the person's name."""
-
 name = ['Alekz', 'Sam', 'Seif', 'Mariez']
 
 print (f'Hey! What time is your next class, {name[0]}?')

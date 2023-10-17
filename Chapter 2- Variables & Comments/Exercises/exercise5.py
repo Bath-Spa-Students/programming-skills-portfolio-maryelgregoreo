8be20d472560be7ea146eee51990e91b03feb82a
maryelgregoreo <maryelgregorio55@gmail.com>
2023-10-17 09:45:27 +0400

    exercise5.py

--- Chapter 2- Variables & Comments/Exercises/exercise5.py
+++ Chapter 2- Variables & Comments/Exercises/exercise5.py
@@ -6,4 +6,4 @@ spent = (int(money/USB))
 left = (int(money-spent))
 
 print (f'She can buy {spent} USB sticks.')
-print (f'The amount she has left is {left}£.')
+print (f'The amount she has left is {left}£.') 

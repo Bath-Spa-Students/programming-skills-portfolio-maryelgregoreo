90abd2d055c5b6c8737340451b5dad7fd25df84b
maryelgregoreo <maryelgregorio55@gmail.com>
2023-11-14 11:38:17 +0400

    exercise1.py

--- Chapter 1- Getting Started with Python/Exercises/exercise1.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise1.py
@@ -2,9 +2,11 @@
 
 # printing "Twinkle, twinkle, little star" in the same format tasked
 
-print("""Twinkle, twinkle, little star,
+print("""
+         Twinkle, twinkle, little star,
              How I wonder what you are!
                  Up above the world so high,
                  Like a diamond in the sky,
          Twinkle, twinkle, little star,
-             How I wonder what you are""")
+             How I wonder what you are
+      """)
